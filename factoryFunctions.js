eef4ba2c201ab268685f6d066b2c61afb1baf9ce
Daniel Landi Macário <landi_daniel@hotmail.com>
2022-02-17 20:30:21 -0300

    Update factoryFunctions.js

---
+++ factoryFunctions.js
@@ -0,0 +1,28 @@
+//  Factory Functions (Função de Fábrica)
+
+const celular = {
+    marcaCelular : 'ASUS',
+    tamanhoTela : {
+        vertical : 155,
+        horizontal : 75
+    },
+    capacidadeBateria: 5000,
+    ligar: function() {
+        console.log("Fazendo Ligação...")
+    }
+}
+
+function criarCelular(marcaCelular,tamanhoTela,capacidadeBateria){
+    return {
+        marcaCelular,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar() {
+            console.log("Fazendo Ligação...")
+        }
+    }
+
+}
+
+const celular1 = criarCelular('Iphone',5.5,5000);
+console.log(celular1);
